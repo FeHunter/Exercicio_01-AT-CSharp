d10e5754b4ccdfbe8b5f575d3c503670441a3ac7
FeHunter <hunterfe1111@gmail.com>
2024-06-15 19:29:11 -0300

    Sequencia Fibonacci feita igual explicação na leitura do AT.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace AtCSharp
     {
         static void Main ()
         {
-            
+            int nMaximo, primeiro, segundo, terceiro;
+
+            Console.Write("\nDigite um valor iniciar a sequência Fibonacci: ");
+            nMaximo = int.Parse(Console.ReadLine());
+
+            primeiro = 0;
+            segundo = 1;
+
+            Console.Write($"{primeiro} {segundo}");
+
+            // Mostra 10 números apartir do número informado.
+            for (int i = 1; i < (nMaximo-1); i++){
+                terceiro = primeiro + segundo;
+                Console.Write($" {terceiro}");
+                primeiro = segundo;
+                segundo = terceiro;
+            }
         }
     }
 }
